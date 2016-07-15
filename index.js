7834562a3968dbd89e85ab97ee790fad9d96593f
Judd Keppel <kepzorz@gmail.com>
2016-07-15 17:51:01 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,33 @@
+'use strict'
+
+const { Module, Tensor } = require('weblearn')
+const old = require('old')
+
+
+class ReLU extends Module {
+  constructor(args) {
+    super()
+  }
+
+  updateOutput(input) {
+    let output = new Tensor(input.values)
+    output.apply((x)=>{
+      return Math.max(0, x)
+    })
+    this.output = output
+    return this.output
+  }
+
+  updateGradInput(input, gradOutput) {
+    this.gradInput = new Tensor(gradOutput.values.map((x, index)=>{
+      if(this.output.values[index] > 0){
+        return x
+      } else {
+        return 0
+      }
+    }))
+    return this.gradInput
+  }
+}
+
+module.exports = old(ReLU)
